71c190864109c5e41f906a398d0af409a5ad8512
Alexandrea Burt <alexlewiseburt@gmail.com>
2022-11-22 22:27:10 -0500

    beginning set up

--- runtime.js
+++ runtime.js
@@ -1,38 +1,30 @@
-const perf = require('execution-time')();
+const perf = require("execution-time")();
 
+function doublerAppend(nums) {
+  let new_nums = [];
 
-function doublerAppend(nums){
-
-    let new_nums = [];
-
-    for (let i=0; i<nums.length; i++){
-        let num = nums[i] * 2;
-        new_nums.push(num);
-    }
-
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i] * 2;
+    new_nums.push(num);
+  }
 }
 
+function doublerInsert(nums) {
+  let new_nums = [];
 
-function doublerInsert(nums){
-
-    let new_nums = [];
-
-    for (let i=0; i<nums.length; i++){
-        let num = nums[i] * 2;
-        new_nums.unshift(num);
-    }
-
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i] * 2;
+    new_nums.unshift(num);
+  }
 }
 
-
-function getSizedArray(size){
-    let array = [];
-    for (let i=0; i<size; i++){
-        array.push(i);
-    }
-    return array
+function getSizedArray(size) {
+  let array = [];
+  for (let i = 0; i < size; i++) {
+    array.push(i);
+  }
+  return array;
 }
-
 
 const tinyArray = getSizedArray(10);
 const smallArray = getSizedArray(100);
@@ -40,23 +32,26 @@ const mediumArray = getSizedArray(1000);
 const largeArray = getSizedArray(10000);
 const extraLargeArray = getSizedArray(100000);
 
+// How long does it take to double every number in a given
+// array?
+const measurePerformance = (array, name) => {
+  // Try it with first function
+  perf.start(); // Starts timer
+  doublerAppend(array);
+  let resultsAppend = perf.stop(); // Stops timer and save time results
 
+  // Try it with second function
+  perf.start();
+  doublerInsert(array);
+  let resultsInsert = perf.stop();
 
-// How long does it take to double every number in a given 
-// array? 
+  console.log(`Results for the ${name}`);
+  console.log("insert", resultsInsert.preciseWords);
+  console.log("append", resultsAppend.preciseWords);
+};
 
-// Try it with first function
-perf.start();                     // Starts timer
-doublerAppend(extraLargeArray);
-let resultsAppend = perf.stop();  // Stops timer and save time results
-
-
-// Try it with second function
-perf.start();
-doublerInsert(extraLargeArray);
-let resultsInsert = perf.stop();
-
-
-console.log('Results for the extraLargeArray');
-console.log("insert", resultsInsert.preciseWords);
-console.log("append", resultsAppend.preciseWords);
+measurePerformance(extraLargeArray, "extraLargeArray");
+measurePerformance(largeArray, "largeArray");
+measurePerformance(mediumArray, "mediumArray");
+measurePerformance(smallArray, "smallArray");
+measurePerformance(tinyArray, "tinyArray");
